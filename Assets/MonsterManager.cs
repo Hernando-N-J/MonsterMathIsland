1d134194e81081ffd30dec2bb59edc739b1ca356
LAPTOP-HERNANDO\Hernando <hernandonj.unity@gmail.com>
2021-12-29 22:17:10 -0500

    Coroutines watched

--- Assets/MonsterManager.cs
+++ Assets/MonsterManager.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using TutorialAssets.Scripts;
 using UnityEngine;
@@ -11,16 +13,34 @@ public class MonsterManager : MonoBehaviour
     [SerializeField] private Transform queuePoint;
     [SerializeField] private GameObject[] monsterPrefabs;
     [SerializeField] private float waveDifficulty;
-    
+
     public List<GameObject> monstersList;
 
     private void Awake()
     {
-        for (int i = 0; i < monstersAmount; i++) InstantiateMonster();
+        // try
+        // {
+        //     for (int i = 0; i < monstersAmount; i++) 
+        //         InstantiateMonster();
+        // }
+        // catch (Exception e)
+        // {
+        //     Debug.LogError($"Exception error in '{gameObject.name}'");
+        // }
+
+        foreach (var m in monsterPrefabs)
+        {
+            if (m == null) Debug.LogError($"Exception error in '{gameObject.name}'");
+        }
+
+        if (monsterSpawnPoint == null) Debug.LogError($"Check {gameObject.name}");
+
+        for (int i = 0; i < monstersAmount; i++)
+            InstantiateMonster();
 
         MonsterAttacks(0);
         MoveNextMonsterToQueue();
-        
+
         // Use ref for returning data from a large collection and avoid variable duplicates
         CalculateWaveDifficulty(ref waveDifficulty);
     }
@@ -28,13 +48,15 @@ public class MonsterManager : MonoBehaviour
     private void InstantiateMonster()
     {
         var monsterRandIndex = Random.Range(0, monsterPrefabs.Length);
-        var monster = Instantiate(monsterPrefabs[monsterRandIndex], monsterSpawnPoint.position, monsterSpawnPoint.rotation);
+        var monster = Instantiate(monsterPrefabs[monsterRandIndex],
+            monsterSpawnPoint.position,
+            monsterSpawnPoint.rotation);
         monstersList.Add(monster);
     }
 
     private void CalculateWaveDifficulty(ref float difficulty)
     {
-        foreach (var monster in monstersList) 
+        foreach (var monster in monstersList)
             difficulty += monster.GetComponent<Points>().points;
 
         difficulty /= (monstersAmount * 3);
@@ -46,18 +68,22 @@ public class MonsterManager : MonoBehaviour
 
         var monsterTransform = monstersList[monsterIndex].transform;
         monsterTransform.GetComponent<MonsterController>().ChangeState(MonsterController.MonsterState.Attack);
-        monsterTransform.position = attackPoint.position;
-        monsterTransform.rotation = attackPoint.rotation;
+        // monsterTransform.position = attackPoint.position;
+        // monsterTransform.rotation = attackPoint.rotation;
+
+        StartCoroutine(LerpToPosition(monsterTransform, attackPoint.position, attackPoint.rotation, 0.5f));
     }
-    
+
     public void MoveMonsterToQueue(int monsterIndex)
     {
         if (monstersList.Count <= monsterIndex) return;
 
         var monsterTransform = monstersList[monsterIndex].transform;
         monsterTransform.GetComponent<MonsterController>().ChangeState(MonsterController.MonsterState.Queue);
-        monsterTransform.position = queuePoint.position;
-        monsterTransform.rotation = queuePoint.rotation;
+        // monsterTransform.position = queuePoint.position;
+        // monsterTransform.rotation = queuePoint.rotation;
+        
+        StartCoroutine(LerpToPosition(monsterTransform, queuePoint.position, queuePoint.rotation, 0.2f));
     }
 
     public void MoveNextMonsterToQueue() => MoveMonsterToQueue(1);
@@ -67,5 +93,20 @@ public class MonsterManager : MonoBehaviour
         // Destroy gameobject and remove it from the list
         Destroy(monstersList[monsterIndex]);
         monstersList.RemoveAt(monsterIndex);
+    }
+
+    private IEnumerator LerpToPosition(Transform objTransform, Vector3 newPosition, Quaternion qRotation, float speed)
+    {
+        var distanceToNewPosition = Vector3.Distance(objTransform.position, newPosition);
+        var timer = 0f;
+        while (distanceToNewPosition > 0.1f)
+        {
+            distanceToNewPosition = Vector3.Distance(objTransform.position, newPosition);
+            objTransform.position = Vector3.Lerp(objTransform.position, newPosition, timer * speed);
+            objTransform.rotation = qRotation;
+
+            timer += Time.deltaTime;
+            yield return null;
+        }
     }
 }
